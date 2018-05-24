00bc386398c2cf2b2e9525984947aab4b4063b97
HanadiBaeissa <31847134+HanadiBaeissa@users.noreply.github.com>
2018-05-24 18:46:53 +0100

    Update LOF-GOF_predicter.R

--- LOF-GOF_predicter.R
+++ LOF-GOF_predicter.R
@@ -1,12 +1,12 @@
-library(e1071)
-
-# load model
-load("svm.model.RData")
+set.seed(12345)
+library(plyr)
+library(randomForest)
+load("rf.model.RData")
 
 # read prediction set
 # it should be noted that Protien ids are repetative so we can not use those as rownames. but we do an alternate way.
 
-pred_set <- read.csv("Features-Insertion-Neutral.csv",header=T)
+pred_set <- read.csv("File.csv",header=T)
 # keep only complete data 
 pred_set <- na.omit(pred_set)
 
@@ -14,22 +14,19 @@ pred_set <- na.omit(pred_set)
 
 ids_mutation <- pred_set[,1:2]
 
-# original model was built using 11 features we need to create data with exactly similiar feature 
+# original model was built using 11 features we need to create data with exactly similiar feature
 pred_data_new <- pred_set[,3:length(pred_set)]
 
 # check dim
 dim(pred_data_new)
 dim(ids_mutation)
 
+
+
 # prediction 
-pred <- predict(object=svm.model,newdata=pred_data_new)
-
-# arrange result 
-
+pred <- predict(object=data.train.rf,newdata=pred_data_new)
 final_pred <- cbind(ids_mutation,pred)
 # reassign column names
 colnames(final_pred) <- c("Protien_ID","Substitution","Prediction")
 # write result
-write.table(final_pred,file="Result.csv",row.names=F)
-
-
+write.table(final_pred,file="final_result_RF.csv",row.names=F)
